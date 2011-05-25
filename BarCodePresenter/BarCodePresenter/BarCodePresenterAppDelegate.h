fe966e86573ed0a36e2c4b261156305467de9647
Milos Djikic <djikicmilos11@gmail.com>
2011-05-25 15:33:59 +0200

    q

---
+++ BarCodePresenter/BarCodePresenter/BarCodePresenterAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BarCodePresenterAppDelegate.h
+//  BarCodePresenter
+//
+//  Created by Zesium on 5/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarCodePresenterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ BarCodePresenter/BarCodePresenter/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BarCodePresenter
+//
+//  Created by Zesium on 5/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
